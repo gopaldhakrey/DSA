89f8e622746366061fdae0d90785f83b7f53a339
Gopal Dhakrey <gopaldhakrey612@gmail.com>
2026-09-09 22:27:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (88.68%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void sortColors(int[]arr) {
+       int n = arr.length;
+       int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i <= n - 1; i++) {
+            if (arr[i] == 0) count0++;
+            else if (arr[i] == 1) count1++;
+            else if(arr[i]==2) count2++;
+        }
+        ;
+
+        for (int i = 0; i < count0; i++) {
+            arr[i] = 0;
+
+        }
+        ;
+        for (int i = count0; i < count0+count1; i++) {
+            arr[i] = 1;
+        }
+        ;
+
+        for (int i = count0+count1; i < n ; i++) {
+            arr[i] = 2;
+        }
+       
+
+
+
+       }
+      
+
+
+
+        
+    };
